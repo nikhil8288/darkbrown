fc629077a333c17173b73234d58b33a0036d8ab5
Nikhil Kumar <nikhil8288@gmail.com>
2026-07-02 14:16:06 +0530

    Update hooks.py

--- darkbrown/hooks.py
+++ darkbrown/hooks.py
@@ -9,8 +9,8 @@ on_session_creation = "darkbrown.auth.on_session_creation"
 
 doc_events = {
     "Building": {
-        "after_insert": "darkbrown.darkbrown.utils.cost_center.create_building_cost_center",
-        "on_update": "darkbrown.darkbrown.utils.cost_center.sync_building_cost_center_rename",
-        "on_trash": "darkbrown.darkbrown.utils.cost_center.handle_building_cost_center_delete",
+        "after_insert": "darkbrown.utils.cost_center.create_building_cost_center",
+        "on_update": "darkbrown.utils.cost_center.sync_building_cost_center_rename",
+        "on_trash": "darkbrown.utils.cost_center.handle_building_cost_center_delete",
     }
 }
