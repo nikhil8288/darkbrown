e6ac34212a7b52e49ea2e80eeda403a8360ff01f
Nikhil Kumar <nikhil8288@gmail.com>
2026-07-02 13:52:45 +0530

    Update hooks.py

--- darkbrown/hooks.py
+++ darkbrown/hooks.py
@@ -6,3 +6,11 @@ app_email = "admin@darkbrown.qa"
 app_license = "MIT"
 
 on_session_creation = "darkbrown.auth.on_session_creation"
+
+doc_events = {
+    "Building": {
+        "after_insert": "darkbrown.darkbrown.utils.cost_center.create_building_cost_center",
+        "on_update": "darkbrown.darkbrown.utils.cost_center.sync_building_cost_center_rename",
+        "on_trash": "darkbrown.darkbrown.utils.cost_center.handle_building_cost_center_delete",
+    }
+}
