37dcde3f0ba569b27a2941ee51cbd6ae574aeb7a
Nikhil Kumar <nikhil8288@gmail.com>
2026-07-12 00:55:58 +0530

    Add files via upload

--- darkbrown/hooks.py
+++ darkbrown/hooks.py
@@ -13,5 +13,15 @@ doc_events = {
         "on_update": "darkbrown.utils.cost_center.sync_building_cost_center_rename",
         "after_rename": "darkbrown.utils.cost_center.sync_building_cost_center_after_rename",
         "on_trash": "darkbrown.utils.cost_center.handle_building_cost_center_delete",
-    }
+    },
+    "Payment Entry": {
+        "on_submit": "darkbrown.utils.collections_case.on_payment_entry_submit",
+    },
+}
+
+scheduler_events = {
+    "daily": [
+        "darkbrown.utils.collections_case.auto_open_cases",
+        "darkbrown.utils.collections_case.reopen_broken_promises",
+    ],
 }
